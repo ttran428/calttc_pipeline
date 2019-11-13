88ae295a3db238f56ebcd50d8612c540c67706a3
Teddy Tran <teddy.theodore.tran@gmail.com>
2019-11-12 23:11:29 -0800

    Implemented Advanced Training (#6)

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# calttc_pipeline
+# CalTTC Pipeline
 Cal Table Tennis automated pipeline for signups.
+
+
+# Setup
+- Setup virtualenv for Python 3.7.4. (Instructions tbd if you don't know how)
+- Clone this repo
+- run `pip install -r requirements.txt` inside your virtualenv
+
+
+# How to Use
+1) Download the sheet from the Google Signup Form in CSV FORM(!!!) for that week.
+2) Move the file into `calttc_pipeline/inputs/`
+3) Run:
+    `python pipeline.py --form=form --input=input_filename --output=output_filename`
+4) The form options are: 'nt', 'at', 'rr'. 
+5) Your file should appear in `calttc_pipeline/outputs/` under the output name.
+
+For example, let's say you are making the advanced training list and it is called `at_week9_input.csv`.
+You want it to show up as `at_week9_output.csv`. Therefore the command you would type is:
+    `python pipeline.py --form=at --input=at_week9_input.csv --output=at_week9_output.csv`
+    
+Notes:
+- Make sure you remember it's in CSV form and that '.csv'
+- Run flake8 before you push
+- Do not commit real input files with people's real SIDs.
---
+++ pipeline.py
@@ -0,0 +1,78 @@
+import click
+import pandas as pd
+
+# Make sure all global variables match the form.
+SID = 'Student ID Number'
+NAME = 'Name (First and Last)'
+EMAIL = 'Email'
+
+
+def valid_input(input_df: pd.DataFrame) -> bool:
+    # Validates input file for all files.
+    return SID in input_df and NAME in input_df and EMAIL in input_df
+
+
+def create_nt(input_df: pd.DataFrame) -> pd.DataFrame:
+    # Create novice training dataframe.
+    pass
+
+
+def create_at(input_df: pd.DataFrame) -> pd.DataFrame:
+    # Create advanced training dataframe.
+    data = []
+    i = 0
+    for index, player in input_df.iterrows():
+        data.append({'Name': player[NAME], 'Email': player[EMAIL]})
+        i += 1
+
+        # create the lines for waitlisted people after
+        if i == 16:
+            data.append({'Name': '', 'Email': ''})
+            data.append({'Name': 'WAITLIST', 'Email': 'WAITLIST'})
+            data.append({'Name': '---------------------',
+                         'Email': '---------------------'})
+
+    output_df = pd.DataFrame(data)
+    return output_df
+
+
+def create_rr(input_df: pd.DataFrame) -> pd.DataFrame:
+    # Create round robin dataframe.
+    pass
+
+
+@click.command()
+@click.option('--form', default='nt',
+              help='nt=Novice Training, at=Advanced training, rr=Round Robin')
+@click.option('--input', default='test_form',
+              help="the filepath to the csv input file")
+@click.option('--output', default='test_form',
+              help="the filepath to the csv output file")
+def main(form: str, input: str, output: str):
+
+    input_df = pd.read_csv(f'./inputs/{input}')
+
+    # Validate input file that it has the required fields
+    if not valid_input(input_df):
+        raise ValueError("Make sure form has fields: "
+                         "SID, Name, and Email formatted correctly.")
+
+    # creates the desired file
+    if form == 'nt':
+        output_df = create_nt(input_df)
+    elif form == 'at':
+        output_df = create_at(input_df)
+    elif form == 'rr':
+        output_df = create_rr(input_df)
+    else:
+        raise ValueError("Please check type again. Options for form are: "
+                         "nt, at, rr. Otherwise not implemented.")
+
+    # index=False means you don't write indices of row
+    # w+ allows you to create file before writing if it doesn't exist
+    output_df.to_csv(f'outputs/{output}', index=False, mode='w+')
+    print("pipeline worked!")
+
+
+if __name__ == '__main__':
+    main()
